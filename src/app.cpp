940e9de741381269b3bed48d597a9b98092dceec
Joseph West <joeywest1414@gmail.com>
2025-10-20 09:22:46 -0500

    using 'using' to increase code clarity

--- src/app.cpp
+++ src/app.cpp
@@ -7,25 +7,29 @@
 
 #include "SDL3_ttf/SDL_ttf.h"
 
+using std::runtime_error;
+using std::unique_ptr;
+using std::format;
+
 App::App(const char* app_name,
          const char* app_version,
          const char* app_identifier) {
     // Init SDL
     if (!SDL_SetAppMetadata(app_name, app_version, app_identifier)) {
         auto error_msg =
-            std::format("Failed to set app metadata: {}", SDL_GetError());
-        throw std::runtime_error(error_msg);
+            format("Failed to set app metadata: {}", SDL_GetError());
+        throw runtime_error(error_msg);
     }
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         auto error_msg =
-            std::format("Failed to initialize SDL: {}", SDL_GetError());
-        throw std::runtime_error(error_msg);
+            format("Failed to initialize SDL: {}", SDL_GetError());
+        throw runtime_error(error_msg);
     }
     if (!SDL_CreateWindowAndRenderer(
             app_name, 960, 720, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-        auto error_msg = std::format(
+        auto error_msg = format(
             "Failed to create SDL window and renderer: {}", SDL_GetError());
-        throw std::runtime_error(error_msg);
+        throw runtime_error(error_msg);
     }
 
     // Init SDL_ttf
@@ -34,14 +38,14 @@ App::App(const char* app_name,
     }
     font = TTF_OpenFont("CousineNerdFont-Regular.ttf", DEFAULT_FONT_SIZE);
     if (!font) {
-        auto error_msg = std::format("Failed to load font: {}", SDL_GetError());
-        throw std::runtime_error(error_msg);
+        auto error_msg = format("Failed to load font: {}", SDL_GetError());
+        throw runtime_error(error_msg);
     }
     text_engine = TTF_CreateRendererTextEngine(renderer);
     if (!text_engine) {
-        auto error_msg = std::format("Failed to create SDL_ttf text engine: {}",
+        auto error_msg = format("Failed to create SDL_ttf text engine: {}",
                                      SDL_GetError());
-        throw std::runtime_error(error_msg);
+        throw runtime_error(error_msg);
     }
 }
 
@@ -72,6 +76,6 @@ void App::Render() {
     SDL_RenderPresent(renderer);
 }
 
-void App::Add(std::unique_ptr<ui::component::Component>&& component) {
+void App::Add(unique_ptr<ui::component::Component>&& component) {
     ui_state.Add(std::move(component));
 }
